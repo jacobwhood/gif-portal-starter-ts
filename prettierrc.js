0bfbe977cc902a75e99072bdb7fea50455df6452
Jacob Hood <jacobwhood@gmail.com>
2021-11-21 14:39:52 -0600

    repo init

---
+++ prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    tabWidth: 4,
+    printWidth: 120,
+    arrowParens: 'always'
+};
